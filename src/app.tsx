ca10a83a86633e20603eafe590565b3bb8fee93d
Ante Šućur <Ante.Sucur@internationalvalueservices.com>
2022-12-04 16:23:35 +0100

    Implement routing

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,23 @@
-import ListEmployeesComponent from "./components/list_employees_component";
+import React from 'react';
+import ListEmployeesComponent from './components/list_employees_component';
+import HeaderComponent from './components/header_component';
+import './app.css';
+import FooterComponent from './components/footer_component';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return (
-      <div className="container">
-        <ListEmployeesComponent/>
-      </div>
-  );
+    return (
+        <Router>
+            <HeaderComponent />
+            <div className="container">
+                <Routes>
+                    <Route path="/" element={<ListEmployeesComponent />} />
+                    <Route path="/employees" element={<ListEmployeesComponent />} />
+                </Routes>
+            </div>
+            <FooterComponent />
+        </Router>
+    );
 }
 
 export default App;
